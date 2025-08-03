f1ebf0cbfdc13bd4c658e11c7c107ff634ce7309
Anurag <anuragsharma5617@gmail.com>
2025-08-03 14:17:49 +0530

    db setup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import cors from 'cors'
-import { connectDb } from './db';
+import { connectDb } from './db/index.js';
+import { configDotenv } from 'dotenv';
 
 const app = express();
 
 app.use(cors())
 
+configDotenv({
+    path : '.env'
+})
 
 app.listen(process.env.PORT, (req,res) => {
     console.log(`Server started on PORT : ${process.env.PORT}`)
