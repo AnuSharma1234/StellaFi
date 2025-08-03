99af8b7a7feb4dd963f17fe49e1b006de8c5c919
Vivian <vynride@gmail.com>
2025-08-03 14:31:36 +0530

    ui: frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="">
+        <h2 className="text-5xl text-center m-4 text-amber-400">StellaFi</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
